96d996b08059bc8a442d469be162d8b46bf3243d
Joël Gomez Caballe <joel.gomez@aropixel.com>
2020-08-10 14:10:45 +0200

    Use configured Field Class in DefaultPageType form

--- Form/Type/DefaultPageType.php
+++ Form/Type/DefaultPageType.php
@@ -4,8 +4,10 @@ namespace Aropixel\PageBundle\Form\Type;
 
 use Aropixel\AdminBundle\Form\Type\Image\Single\ImageType;
 use Aropixel\PageBundle\Entity\Field;
+use Aropixel\PageBundle\Entity\FieldInterface;
 use Aropixel\PageBundle\Entity\Page;
 use Aropixel\PageBundle\Entity\PageInterface;
+use Aropixel\PageBundle\Form\DataMapper\PageFieldDataMapper;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -20,15 +22,36 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class DefaultPageType extends AbstractPageType
 {
+    /** @var array */
+    private $entities;
+
+
+    /**
+     * DefaultPageType constructor.
+     * @param AuthorizationCheckerInterface $securityContext
+     * @param PageFieldDataMapper $pageFieldDataMapper
+     * @param ParameterBagInterface $parameterBag
+     * @param array $entities
+     */
+    public function __construct(AuthorizationCheckerInterface $securityContext, PageFieldDataMapper $pageFieldDataMapper, ParameterBagInterface $parameterBag, array $entities)
+    {
+        parent::__construct($securityContext, $pageFieldDataMapper, $parameterBag);
+        $this->entities = $entities;
+    }
+
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        //
+        $fieldClass = $this->entities[FieldInterface::class];
+
+        //
         parent::buildForm($builder, $options);
         $builder
             ->add('excerpt', null, array('label'  => 'Chapeau'))
             ->add('description', TextareaType::class, array('label'  => 'Description', 'attr' => array('class' => 'ckeditor')))
             ->add('image', ImageType::class, [
-                'data_class' => Field::class,
+                'data_class' => $fieldClass,
                 'data_value' => 'value',
                 'library' => self::class
             ])
