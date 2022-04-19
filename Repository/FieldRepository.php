f8de414248f13b562dba9b3b45395bfc82a6e5cf
David Robert <contact.davidrobert@gmail.com>
2022-04-19 10:00:20 +0200

    fix field repository dependencies

--- Repository/FieldRepository.php
+++ Repository/FieldRepository.php
@@ -4,7 +4,7 @@ namespace Aropixel\PageBundle\Repository;
 
 use Aropixel\PageBundle\Entity\Field;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Field|null find($id, $lockMode = null, $lockVersion = null)
