b8d633f40f5bbba6c9154c9305df97e68db90d2f
Joël Gomez Caballe <joel.gomez@aropixel.com>
2025-02-23 19:25:38 +0100

    Compatibilité flysystem (#5)
    * Compatibilité flysystem
    
    * Bugfix dto

--- src/EventListener/DoFileCropListener.php
+++ src/EventListener/DoFileCropListener.php
@@ -4,12 +4,16 @@ namespace Aropixel\PageBundle\EventListener;
 
 use Aropixel\AdminBundle\Domain\Media\Image\Crop\CropApplierInterface;
 use Aropixel\AdminBundle\Entity\AttachedImageInterface;
+use Aropixel\AdminBundle\Entity\Image;
 use Aropixel\PageBundle\Entity\FieldInterface;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Event\PostUpdateEventArgs;
 use Doctrine\ORM\Events;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
+use Psr\Log\LoggerInterface;
 
 
 #[AsDoctrineListener(event: Events::postPersist)]
@@ -18,8 +22,11 @@ class DoFileCropListener
 {
     /**
      */
-    public function __construct(private readonly CropApplierInterface $cropper)
-    {
+    public function __construct(
+        private readonly CropApplierInterface $cropper,
+        private readonly FilesystemOperator $privateStorage,
+        private readonly LoggerInterface $logger,
+    ) {
     }
 
     public function postUpdate(PostUpdateEventArgs $args)
@@ -36,22 +43,27 @@ class DoFileCropListener
     {
         $entity = $args->getObject();
 
-        //
         if ($entity instanceof FieldInterface) {
 
-            /** @var AttachedImageInterface $entity */
             $fileName = $entity->getFilename();
             if (is_null($fileName))    return;
-
 
             /** @var FieldInterface $entity */
             $crops = $entity->getCrops();
             if ($crops && is_array($crops)) {
                 foreach ($crops as $crop) {
 
-                    //
-                    $this->cropper->applyCrop($fileName, $crop['filter'], $crop['crop']);
-
+                    try {
+                        $contents = $this->privateStorage->read(Image::UPLOAD_DIR . '/' . $fileName);
+                        [$width, $height] = getimagesizefromstring($contents);
+                        $dtoImage = new Image();
+                        $dtoImage->setWidth($width);
+                        $dtoImage->setHeight($height);
+                        $dtoImage->setFilename($fileName);
+                        $this->cropper->applyCrop($fileName, $crop['filter'], $crop['crop']);
+                    } catch (FilesystemException) {
+                        $this->logger->error(sprintf('Unable to get image size: %s', Image::UPLOAD_DIR . '/' . $fileName));
+                    }
                 }
             }
 
